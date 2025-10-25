f61b29eb700f87a45c1dd3f28fd2da1c5c8aa83f
Mahfujur Rahman <mahfujurrahman1319@gmail.com>
2025-10-25 20:01:11 +0600

    Create problem-A.cpp

---
+++ problem-A.cpp
@@ -0,0 +1,16 @@
+//https://vjudge.net/problem/CodeChef-SUNNYDAY
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int ms;cin>>ms;
+	while(ms--){
+	    int x;cin>>x;
+	    if(x>24){
+	        cout<<"YES"<<endl;	        
+	    }
+	    else cout<<"NO"<<endl;
+	}
+	return 0;
+
+}
