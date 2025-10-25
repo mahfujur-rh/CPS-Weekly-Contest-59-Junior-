1521c7fa1a33e053f32ab59d2cf10e2310613d1b
Mahfujur Rahman <mahfujurrahman1319@gmail.com>
2025-10-25 20:55:52 +0600

    Create problem-D.cpp

---
+++ problem-D.cpp
@@ -0,0 +1,20 @@
+//https://atcoder.jp/contests/abc333/tasks/abc333_a
+//https://vjudge.net/problem/AtCoder-abc333_a
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int n;cin>>n;         /*int x=1;
+                      	while(x<=n){
+                      	  cout<<n;
+                      	  x++;
+                          */
+
+	for(int i=1;i<=n;i++)
+  {
+	    cout<<n;
+	}
+	return 0;
+
+}
