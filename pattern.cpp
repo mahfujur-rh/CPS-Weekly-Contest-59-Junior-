6df804ade8ebd5e4d15364f33ab382382a30ddfa
Mahfujur Rahman <mahfujurrahman1319@gmail.com>
2025-11-23 19:41:38 +0600

    Update pattern.cpp

--- pattern.cpp
+++ pattern.cpp
@@ -5,9 +5,13 @@ int main() {
 	for(int i=0;i<=6;i++){
 	    int spaces=6-i;
 	    int star=i;
+		//for(int j=1;j<=6-i;j++){
+		//cout<<" ";}
 	    while(spaces--){
 	        cout<<" ";
 	    }
+
+		//for(intj=1;j<=i;j++){cout<<"*";}
 	    while(star--){
 	        cout<<"*";
 	    }
